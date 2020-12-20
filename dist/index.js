1bbfd803c50e99f1b474231a0549a9264a70deb3
Shih-Min Lee <ssmlee04@gmail.com>
2020-12-20 18:20:45 -0500

    able to calcualte some bar charts

--- dist/index.js
+++ dist/index.js
@@ -3,19 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = exports.NumberOfEmployees = void 0;
+exports["default"] = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
 var _lodash = _interopRequireDefault(require("lodash"));
 
 var _reactChartjs = require("react-chartjs-2");
-
-var _reactCopyToClipboard = require("react-copy-to-clipboard");
-
-var _dayjs = _interopRequireDefault(require("dayjs"));
-
-var _dayjsPluginUtc = _interopRequireDefault(require("dayjs-plugin-utc"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
@@ -37,213 +31,113 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
-_dayjs["default"].extend(_dayjsPluginUtc["default"]);
+var options = {
+  legend: {
+    display: false
+  },
+  scales: {
+    xAxes: [{
+      ticks: {
+        autoSkip: false
+      },
+      barPercentage: 0.4
+    }],
+    yAxes: [{
+      type: 'linear',
+      display: true,
+      position: 'right',
+      id: 'earnings',
+      gridLines: {
+        display: false
+      },
+      ticks: {
+        fontColor: 'rgba(138,17,31,0.8)'
+      }
+    }]
+  }
+};
 
-var NumberOfEmployees =
+var TagBarCharts =
 /*#__PURE__*/
 function (_React$Component) {
-  _inherits(NumberOfEmployees, _React$Component);
+  _inherits(TagBarCharts, _React$Component);
 
-  function NumberOfEmployees(props) {
-    var _this;
+  function TagBarCharts() {
+    _classCallCheck(this, TagBarCharts);
 
-    _classCallCheck(this, NumberOfEmployees);
-
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(NumberOfEmployees).call(this, props));
-    _this.state = {};
-    return _this;
+    return _possibleConstructorReturn(this, _getPrototypeOf(TagBarCharts).apply(this, arguments));
   }
 
-  _createClass(NumberOfEmployees, [{
-    key: "shouldComponentUpdate",
-    value: function shouldComponentUpdate(nextProps, nextState) {
-      var profile = this.props.profile;
-      if (!profile) return true;
-      if (nextState.copied) return true;
-      if (profile.ticker !== nextProps.profile.ticker) return true;
-      return false;
-    }
-  }, {
+  _createClass(TagBarCharts, [{
     key: "render",
     value: function render() {
-      var _this2 = this;
-
       var _this$props = this.props,
-          profile = _this$props.profile,
-          _this$props$imgProp = _this$props.imgProp,
-          imgProp = _this$props$imgProp === void 0 ? 'num_employees' : _this$props$imgProp;
-      var copied = this.state.copied;
+          data = _this$props.data,
+          name = _this$props.name,
+          color = _this$props.color,
+          tickercolor = _this$props.tickercolor,
+          _this$props$height = _this$props.height,
+          height = _this$props$height === void 0 ? 80 : _this$props$height,
+          ticker = _this$props.ticker;
 
-      if (!profile) {
+      if (!data) {
         return _react["default"].createElement("div", {
-          style: {
-            fontSize: 8
-          }
+          className: "font-12"
         }, "Not available at this time... ");
       }
 
-      if (profile[imgProp] && profile[imgProp].url) {
-        var btnClass = copied ? 'react-components-show-url btn btn-sm btn-danger disabled font-8' : 'react-components-show-url btn btn-sm btn-warning font-8';
-        var btnText = copied ? 'Copied' : 'Copy Img';
-        return _react["default"].createElement("div", {
-          className: "react-components-show-button"
-        }, _react["default"].createElement("img", {
-          alt: "".concat(profile.ticker, " - ").concat(profile.name, " Employees and Productivity"),
-          src: profile[imgProp].url,
-          style: {
-            width: '100%'
-          }
-        }), _react["default"].createElement(_reactCopyToClipboard.CopyToClipboard, {
-          text: profile[imgProp].url || '',
-          onCopy: function onCopy() {
-            return _this2.setState({
-              copied: true
-            });
-          }
-        }, _react["default"].createElement("button", {
-          className: btnClass,
-          value: btnText
-        }, btnText)));
-      }
+      var dataSorted = _lodash["default"].sortBy(_lodash["default"].filter(data, function (d) {
+        return d.value;
+      }), function (d) {
+        return d.value;
+      });
 
-      if (!profile || !profile.numbers || !profile.numbers.number_of_employees_ts) return null;
-      if (!profile || !profile.numbers || !profile.numbers.revenue_per_employee_ts) return null;
-      var number_of_employees_ts = profile.numbers.number_of_employees_ts || [];
-      var revenue_per_employee_ts = profile.numbers.revenue_per_employee_ts || [];
-      var number_of_employees = number_of_employees_ts.map(function (d) {
-        return d.v;
-      });
-      var revenue_per_employee = revenue_per_employee_ts.map(function (d) {
-        return d.v / 1000;
-      });
-      var data = {
-        labels: number_of_employees_ts.map(function (d) {
-          return _dayjs["default"].utc(d.ts).format('YYYYMM');
+      var chartData = {
+        labels: dataSorted.map(function (d) {
+          return d.ticker;
         }),
         datasets: [{
-          yAxisID: '1',
-          type: 'line',
-          fill: false,
-          backgroundColor: 'crimson',
-          borderColor: 'crimson',
-          pointBackgroundColor: 'white',
-          lineTension: 0.3,
-          borderWidth: 1,
-          pointRadius: 2,
-          pointHoverRadius: 2,
-          data: revenue_per_employee,
-          label: 'Revenue Per Employee (k)'
-        }, {
-          yAxisID: '2',
-          type: 'bar',
-          fill: false,
-          backgroundColor: 'darkgray',
-          borderColor: 'darkgray',
-          pointBackgroundColor: 'white',
-          lineTension: 0.3,
-          borderWidth: 1,
-          pointRadius: 2,
-          pointHoverRadius: 2,
-          data: number_of_employees,
-          label: 'Number Of Employees'
+          borderWidth: 2,
+          // borderColor: color,
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          // pointBorderColor: 'red',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 3,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(0,100,0,1)',
+          // pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 0,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          yAxisID: 'earnings',
+          data: dataSorted.map(function (d) {
+            return d.value;
+          }),
+          backgroundColor: dataSorted.map(function (d) {
+            return ticker && d.ticker !== ticker ? color : tickercolor;
+          })
         }]
       };
-
-      var max = _lodash["default"].max(number_of_employees);
-
-      var min = _lodash["default"].min(number_of_employees);
-
-      var barChartMin = max === min ? max / 2 : Math.max(2 * min - max, 0);
-      var barChartMax = max === min ? max * 1.1 : max + (max - min) * 0.2;
-      var options = {
-        legend: {
-          labels: {
-            fontSize: 8,
-            boxWidth: 10
-          }
-        },
-        scales: {
-          xAxes: [{
-            ticks: {
-              fontSize: 8
-            },
-            barPercentage: 0.4
-          }],
-          yAxes: [{
-            type: 'linear',
-            display: true,
-            position: 'left',
-            id: '1',
-            gridLines: {
-              display: false
-            },
-            labels: {
-              show: true
-            },
-            ticks: {
-              fontColor: 'darkred',
-              fontSize: 8,
-              callback: function callback(label, index, labels) {
-                return Math.floor(label);
-              }
-            }
-          }, {
-            type: 'linear',
-            display: true,
-            position: 'right',
-            id: '2',
-            labels: {
-              show: true
-            },
-            ticks: {
-              fontColor: 'gray',
-              fontSize: 8,
-              max: barChartMax,
-              min: barChartMin,
-              callback: function callback(label, index, labels) {
-                return Math.floor(label);
-              }
-            }
-          }]
-        }
-      };
       return _react["default"].createElement("div", {
-        style: {
-          width: '100%',
-          padding: 5,
-          fontSize: 8
-        }
+        className: "row no-gutters font-10"
       }, _react["default"].createElement("div", {
-        style: {
-          color: 'darkred',
-          fontWeight: 'bold'
-        }
-      }, profile.ticker, " - ", profile.name, " ", _react["default"].createElement("span", {
-        className: "green"
-      }, "Employees and Productivity")), _react["default"].createElement(_reactChartjs.Bar, {
-        data: data,
-        height: 170,
-        options: options
-      }), _react["default"].createElement("div", {
-        style: {
-          fontSize: 8
-        }
-      }, "Generated by ", _react["default"].createElement("span", {
-        style: {
-          color: 'darkred'
-        }
-      }, "@earningsfly"), " with ", _react["default"].createElement("span", {
-        style: {
-          fontSize: 16,
-          color: 'red'
-        }
-      }, "\uD83D\uDE80")));
+        className: "col-md-12",
+        key: name
+      }, _react["default"].createElement("span", {
+        className: "font-12 darkred bold"
+      }, name), _react["default"].createElement(_reactChartjs.Bar, {
+        options: options,
+        data: chartData,
+        height: height
+      })));
     }
   }]);
 
-  return NumberOfEmployees;
+  return TagBarCharts;
 }(_react["default"].Component);
 
-exports.NumberOfEmployees = NumberOfEmployees;
-var _default = NumberOfEmployees;
+var _default = TagBarCharts;
 exports["default"] = _default;
--- src/index.js
+++ src/index.js
@@ -1,152 +1,79 @@
 import React from "react";
 import _ from "lodash";
 import { Bar } from 'react-chartjs-2';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import dayjs from 'dayjs';
-import dayjsPluginUTC from 'dayjs-plugin-utc';
-dayjs.extend(dayjsPluginUTC);
 
-export class NumberOfEmployees extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const options = {
+  legend: {
+    display: false,
+  },
+  scales: {
+    xAxes: [ {
+      ticks: {
+        autoSkip: false,
+      },
+      barPercentage: 0.4,
+    } ],
+    yAxes: [
+      {
+        type: 'linear',
+        display: true,
+        position: 'right',
+        id: 'earnings',
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          fontColor: 'rgba(138,17,31,0.8)',
+        },
+      },
+    ],
+  },
+};
 
-    };
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    const { profile } = this.props;
-    if (!profile) return true;
-    if (nextState.copied) return true;
-    if (profile.ticker !== nextProps.profile.ticker) return true;
-    return false;
-  }
-
+class TagBarCharts extends React.Component {
   render() {
-    const { profile, imgProp = 'num_employees' } = this.props;
-    const { copied } = this.state;
-    if (!profile) {
+    const { data, name, color, tickercolor, height = 80, ticker } = this.props;
+    if ( !data ) {
       return (
-        <div style={{ fontSize: 8 }}>Not available at this time... </div>
+        <div className='font-12'>Not available at this time... </div>
       );
     }
-    if (profile[imgProp] && profile[imgProp].url) {
-      const btnClass = copied ? 'react-components-show-url btn btn-sm btn-danger disabled font-8' : 'react-components-show-url btn btn-sm btn-warning font-8';
-      const btnText = copied ? 'Copied' : 'Copy Img';
-      return (
-        <div className='react-components-show-button'>
-          <img alt={`${profile.ticker} - ${profile.name} Employees and Productivity`} src={profile[imgProp].url} style={{ width: '100%' }} />
-          <CopyToClipboard text={profile[imgProp].url || ''}
-            onCopy={() => this.setState({ copied: true })}
-          >
-            <button className={btnClass} value={btnText}>{btnText}</button>
-          </CopyToClipboard>
-        </div>
-      );
-    }
-
-    if (!profile || !profile.numbers || !profile.numbers.number_of_employees_ts) return null;
-    if (!profile || !profile.numbers || !profile.numbers.revenue_per_employee_ts) return null;
-    const number_of_employees_ts = profile.numbers.number_of_employees_ts || [];
-    const revenue_per_employee_ts = profile.numbers.revenue_per_employee_ts || [];
-    const number_of_employees = number_of_employees_ts.map(d => d.v);
-    const revenue_per_employee = revenue_per_employee_ts.map(d => d.v / 1000);
-    const data = {
-      labels: number_of_employees_ts.map(d => dayjs.utc(d.ts).format('YYYYMM')),
-      datasets: [{
-        yAxisID: '1',
-        type: 'line',
-        fill: false,
-        backgroundColor: 'crimson',
-        borderColor: 'crimson',
-        pointBackgroundColor: 'white',
-        lineTension: 0.3,
-        borderWidth: 1,
-        pointRadius: 2,
-        pointHoverRadius: 2,
-        data: revenue_per_employee,
-        label: 'Revenue Per Employee (k)'
-      }, {
-        yAxisID: '2',
-        type: 'bar',
-        fill: false,
-        backgroundColor: 'darkgray',
-        borderColor: 'darkgray',
-        pointBackgroundColor: 'white',
-        lineTension: 0.3,
-        borderWidth: 1,
-        pointRadius: 2,
-        pointHoverRadius: 2,
-        data: number_of_employees,
-        label: 'Number Of Employees'
-      }]
-    };
-    const max = _.max(number_of_employees);
-    const min = _.min(number_of_employees);
-    const barChartMin = max === min ? max / 2 : Math.max(2 * min - max, 0);
-    const barChartMax = max === min ? max * 1.1 : max + (max - min) * 0.2;
-    const options = {
-      legend: {
-        labels: {
-          fontSize: 8,
-          boxWidth: 10,
-        }
-      },
-      scales: {
-        xAxes: [{
-          ticks: {
-            fontSize: 8
-          },
-          barPercentage: 0.4
-        }],
-        yAxes: [{
-                type: 'linear',
-                display: true,
-                position: 'left',
-                id: '1',
-                gridLines: {
-                  display: false
-                },
-                labels: {
-                  show: true
-                },
-                ticks: {
-                  fontColor: 'darkred',
-                  fontSize: 8,
-                    callback: function(label, index, labels) {
-                      return Math.floor(label);
-                    }
-                },
-              },
-              {
-                type: 'linear',
-                display: true,
-                position: 'right',
-                id: '2',
-                labels: {
-                  show: true
-                },
-                ticks: {
-                  fontColor: 'gray',
-                  fontSize: 8,
-                  max: barChartMax,
-                  min: barChartMin,
-                  callback: function(label, index, labels) {
-                    return Math.floor(label);
-                  }
-                },
-              }]
-      },
+    const dataSorted = _.sortBy(_.filter(data, d => d.value), d => d.value);
+    const chartData = {
+      labels: dataSorted.map( d => d.ticker ),
+      datasets: [
+        {
+          borderWidth: 2,
+          // borderColor: color,
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          // pointBorderColor: 'red',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 3,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(0,100,0,1)',
+          // pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 0,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          yAxisID: 'earnings',
+          data: dataSorted.map( d => d.value ),
+          backgroundColor: dataSorted.map( d => (ticker && d.ticker !== ticker ? color : tickercolor) )
+        },
+      ],
     };
 
     return (
-      <div style={{ width: '100%', padding: 5, fontSize: 8 }}>
-        <div style={{ color: 'darkred', fontWeight: 'bold' }}>{profile.ticker} - {profile.name} <span className='green'>Employees and Productivity</span></div>
-        <Bar data={data} height={170} options={options} />
-        <div style={{ fontSize: 8 }}>Generated by <span style={{ color: 'darkred' }}>@earningsfly</span> with <span style={{ fontSize: 16, color: 'red' }}>🚀</span></div>
+      <div className='row no-gutters font-10'>
+        <div className='col-md-12' key={name}>
+          <span className='font-12 darkred bold'>{name}</span>
+          <Bar options={options} data={chartData} height={height} />
+        </div>
       </div>
     );
   }
 }
 
-export default NumberOfEmployees;
+export default TagBarCharts;
